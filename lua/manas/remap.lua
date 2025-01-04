5c893e3f0323d21ef96b9b79b1edeb508ad604fd
manaslahon <manaslahonworking@gmail.com>
2025-01-04 13:00:33 +0530

    add remap for $ and _

--- lua/manas/remap.lua
+++ lua/manas/remap.lua
@@ -1,11 +1,13 @@
 vim.g.mapleader = '\\'
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") --move codeblocks or lines up or down
-vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 --remap existing maps
 vim.keymap.set("i", "jj", "<Esc>", {noremap = true, silent = true})
+vim.keymap.set("n", "<C-d>", "$", {noremap = true, silent = true})
+vim.keymap.set("n", "<C-f>", "_", {noremap = true, silent = true})
 
 --yank to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
